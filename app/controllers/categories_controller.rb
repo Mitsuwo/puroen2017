bea922fb4903dd50e02441431bec4b6f3400c711
Mitsuwo <hapowalp@gmail.com>
2017-12-13 15:15:42 +0900

    change category layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
   def category
   end
 
+  def show
+    @books = Book.category.find(params[:id])
+  end
+
   def index
     @category = Category.find(params[:id])
   end
