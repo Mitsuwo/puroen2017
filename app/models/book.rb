76e524757175dbb9a66d336e99043c45c1d95d96
Mitsuwo <hapowalp@gmail.com>
2017-12-19 20:51:19 +0900

    japanese localization

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :category
-  has_one: choose
+  has_one :choose
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one: choose
+  has_one :choose
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
